00520bf286e013e60cbf307490ec3a9750640e40
tianwei <tianweia@mail.taiji.com.cn>
2024-08-26 16:36:50 +0800

    新增TextRank算法服务

--- app/key_extract.py
+++ app/key_extract.py
@@ -1,13 +1,22 @@
-from model.tf_idf import TFIDF
+from model.graph_model.text_rank import TextRank
+from model.statistical_model.tf_idf import TFIDF
 
 
 class KeywordExtract(object):
     def __init__(self):
         self.tf_idf = TFIDF()
+        self.text_rank = TextRank()
 
     def infer(self, infer_list, type='TF-IDF'):
+        """
+
+        :param infer_list:
+        :param type:
+        :return:
+        """
+        result = []
         if type == "TF-IDF":
             result = self.tf_idf.infer(infer_list)
-            return result
-
-
+        if type == "TextRank":
+            result = self.text_rank.infer(infer_list)
+        return result
