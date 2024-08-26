b7a64e9df9a25e8ac197cfabfada52699089def4
tianwei <tianweia@mail.taiji.com.cn>
2024-08-26 16:37:09 +0800

    新增对TextRank算法的调用实例

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from app.key_extract import KeywordExtract
 
 if __name__ == '__main__':
-
-    input_list = ["this is a sample",
-                  "this is another example example example",
-                  "this is a different example example"]
+    input_list = ["this is another example example example"]
     key_extract = KeywordExtract()
-
+    # 基于TF-IDF进行关键词的抽取
     print(key_extract.infer(input_list))
+    # 基于TextRank 进行关键词的抽取
+    print(key_extract.infer(input_list, type="TextRank"))
