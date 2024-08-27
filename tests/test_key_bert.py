1f57836f90d73da9f3e32293a85048f6dbc97bd1
tianwei <tianweia@mail.taiji.com.cn>
2024-08-27 10:59:19 +0800

    三个模型（tfidf、textrank、keybert）测试用例

--- tests/test_key_bert.py
+++ tests/test_key_bert.py
@@ -1,8 +1,7 @@
-from keyword_extract.statistical_model.tf_idf import TFIDF
+from keyword_extract.keybert_model.key_bert import KeyBERT
 
 if __name__ == '__main__':
     input_list = ["自然语言处理是人工智能领域中的一个重要方向。它研究人与计算机之间如何使用自然语言进行有效沟通。"]
-    tfidf = TFIDF()
-    # 基于TF-IDF进行关键词的抽取
-    print(tfidf.infer(input_list))
-
+    key_bert = KeyBERT()
+    # 基于TextRank 进行关键词的抽取
+    print(key_bert.infer(input_list))
--- tests/test_text_rank.py
+++ tests/test_text_rank.py
@@ -3,7 +3,5 @@ from keyword_extract.graph_model.text_rank import TextRank
 if __name__ == '__main__':
     input_list = ["自然语言处理是人工智能领域中的一个重要方向。它研究人与计算机之间如何使用自然语言进行有效沟通。"]
     text_rank = TextRank()
-    # 基于TF-IDF进行关键词的抽取
-    print(text_rank.infer(input_list))
     # 基于TextRank 进行关键词的抽取
     print(text_rank.infer(input_list))
