13a0204bc295ed1849f920dfa06f21028d9ea310
tianwei <tianweia@mail.taiji.com.cn>
2024-08-27 10:48:40 +0800

    增加key_bert的config

--- keyword_extract/config.py
+++ keyword_extract/config.py
@@ -1,6 +1,14 @@
 import os
+import torch
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 
 STOP_PATH = os.path.join(DATA_DIR, 'stop.txt')
+PRE_MODEL = os.path.join(DATA_DIR, 'pre_model')
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class KeyBERTConfig(object):
+    BERT_MODEL = os.path.join(PRE_MODEL, 'bert_base_chinese')
